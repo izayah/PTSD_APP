0223cb6f0c33f956bda92013599940168acfdc8e
izayah <izayahallen@gmail.com>
2016-06-26 15:34:53 -0500

    Created Stress Tools Activity

---
+++ ptsd/src/main/java/com/example/ptsd/StressToolsActivity.java
@@ -0,0 +1,13 @@
+package com.example.ptsd;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class StressToolsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stress_tools);
+    }
+}
