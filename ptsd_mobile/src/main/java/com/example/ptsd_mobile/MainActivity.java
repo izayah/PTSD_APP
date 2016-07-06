d2b6822dfc4be3d3661951bb808347e06ac1494b
bandishthaker <bandish.9999@gmail.com>
2016-07-06 00:20:44 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ptsd/src/main/java/com/example/ptsd/AssessmentActivity.java
    #	ptsd/src/main/res/layout/activity_assessment.xml

--- ptsd_mobile/src/main/java/com/example/ptsd_mobile/MainActivity.java
+++ ptsd_mobile/src/main/java/com/example/ptsd_mobile/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.ptsd.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
